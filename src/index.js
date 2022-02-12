49e9afeec39ee28b79667a92aeafc7ca3725eef7
Paul Anigbo <12402791+wizzypro@users.noreply.github.com>
2022-02-11 18:47:35 -0600

    converted index functions to arrow functions

--- src/index.js
+++ src/index.js
@@ -4,27 +4,28 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+const newNumbers = numbers.map((x) => x * 2);
+
+console.log(newNumbers);
 
 //////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+const newNumbers1 = numbers.filter((num) => num < 10);
+
+console.log(newNumbers1);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+var newNumber = numbers.reduce((accumulator, currentNumber) => {
+  return accumulator + currentNumber;
+});
 
+console.log(newNumber);
 ////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+const newNumber2 = numbers.find((num) => num > 10);
+
+console.log(newNumber2);
 
 ////FindIndex - find the index of the first item that matches.
 // const newNumber = numbers.findIndex(function (num) {
